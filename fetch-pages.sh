21023fb6348f224d91b991e8f6a258c83b79a38d
Frank Hang <frankzhang123@gmail.com>
2018-04-02 16:46:19 +0800

    Normalize the sequence of arguments

--- fetch-pages.sh
+++ fetch-pages.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+DIR=`dirname $0`
 curl -L -c giga-web.cookie.jar https://www.giga-web.jp/cookie_set.php > cookie_set.php
 
 for i in `seq $2 $1`; do
 	echo get $i
-	curl -b giga-web.cookie.jar 'https://www.giga-web.jp/product/index.php?menu=1&product_id='$i > $i.html
+	curl -b giga-web.cookie.jar 'https://www.giga-web.jp/product/index.php?menu=1&product_id='$i > ${DIR}/data/$i.html
 done
